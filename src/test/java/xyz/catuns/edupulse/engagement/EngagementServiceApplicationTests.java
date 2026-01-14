8553348f7855f6d2b93b4c14927611ac650a2c1f
Devin Catuns <devincatuns1@gmail.com>
2026-01-13 20:55:20 -0500

    first commit

---
+++ src/test/java/xyz/catuns/edupulse/engagement/EngagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.catuns.edupulse.engagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
